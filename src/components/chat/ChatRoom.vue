1899e944b76d55f644ed0132875af1d7a60b896c
infinityAlive <sonic@ehanlin.com.tw>
2019-03-11 18:46:03 +0800

    使用者傳送圖片時，可產生小尺寸之圖片檔案，用於 push 至 linebot 之預覽圖案

--- src/components/chat/ChatRoom.vue
+++ src/components/chat/ChatRoom.vue
@@ -12,10 +12,10 @@
             </mu-avatar>
             {{ !messageInfo.ehanlin ? messageInfo.lineUserName : messageInfo.ehanlinName}}
           </div>
-          <a v-if="messageInfo.imageUrl" :href="messageInfo.imageUrl" target="_blank">
-            <img class="chat-image" v-if="messageInfo.imageUrl" :src="messageInfo.imageUrl">
+          <a v-show="messageInfo.imageUrl" :href="messageInfo.imageUrl" target="_blank">
+            <img class="chat-image" :src="messageInfo.imageUrl">
           </a>
-          <article v-if="!messageInfo.imageUrl"
+          <article v-show="!messageInfo.imageUrl"
                    :class="!messageInfo.ehanlin ? 'dialog-block-user' : 'dialog-block-ehanlin'">
             <template v-for="line in messageInfo.text.split('\n')">{{line}}<br></template>
           </article>
@@ -24,23 +24,31 @@
     </article>
     <article id="dialog-input">
       <mu-container>
-        <mu-row gutter style="background-color: #f1f7fe;">
-          <mu-col span="11" style="min-height: 80px;" v-show="isPreviewImage">
-            <mu-circular-progress style="margin-top: 20px;" :size="36"
-                                  v-show="isShowImageProgress"></mu-circular-progress>
-            <img class="chat-image-preview" :src="previewImageUrl">
-          </mu-col>
-          <mu-col span="11" v-show="!isPreviewImage">
-            <mu-text-field v-model="messageText"
-                           multi-line full-width :rows="3" :rows-max="5"
-                           @keypress.enter.exact="submitMessage"
-                           underline-color="#004ec4">
-            </mu-text-field>
-          </mu-col>
-          <mu-col span="1">
-            <input id="upload-image" type="file" accept="image/*" @change="uploadImage($event)" style="display: none;">
-            <mu-icon size="35" value="image" class="trigger-upload-image" @click="triggerUploadImage"></mu-icon>
-          </mu-col>
+        <mu-row style="background-color: #f1f7fe;">
+          <section style="width: 75%;" v-show="!isPreviewImage">
+            <mu-flex fill justify-content="start">
+              <mu-text-field v-model="messageText"
+                             multi-line full-width :rows="3" :rows-max="5"
+                             @keypress.enter.exact="sentMessage"
+                             underline-color="#004ec4">
+              </mu-text-field>
+            </mu-flex>
+          </section>
+          <section style="width: 75%; min-height: 80px;" v-show="isPreviewImage">
+            <mu-flex fill justify-content="start">
+              <mu-circular-progress style="margin-top: 20px;" :size="36"
+                                    v-show="isShowImageProgress"></mu-circular-progress>
+              <img id="preview-image" class="chat-image-preview" :src="originalImageUrl">
+            </mu-flex>
+          </section>
+          <mu-flex class="flex-demo" fill justify-content="end">
+            <input id="upload-image" type="file" accept="image/*" @change="paintCanvas($event)" style="display: none;">
+            <mu-icon class="image-control-icon" v-show="originalImageUrl" size="30" value="delete_forever"
+                     @click="cancelImage"></mu-icon>
+            <mu-icon class="image-control-icon" v-show="originalImageUrl" size="27" value="send"
+                     @click="sentImage"></mu-icon>
+            <mu-icon class="image-control-icon" size="27" value="image" @click="triggerUploadImage"></mu-icon>
+          </mu-flex>
         </mu-row>
       </mu-container>
     </article>
@@ -51,11 +59,14 @@
   import eventBus from '../../modules/event-bus'
   import { firebase, db, storage } from '../../modules/firebase-config'
   import { mapState } from 'vuex'
+  import { showModal } from '../../modules/modal'
 
   export default {
     name: 'ChatRoom',
     data () {
       return {
+        textColSpan: 11,
+        imageColSpan: 9,
         lineUserName: '',
         lineUserAvatar: '',
         messageText: '',
@@ -64,7 +75,8 @@
         storageRef: Object,
         isPreviewImage: false,
         isShowImageProgress: false,
-        previewImageUrl: ''
+        originalImageUrl: '',
+        smallImageUrl: ''
       }
     },
 
@@ -88,6 +100,102 @@
     },
 
     methods: {
+      scrollBottom () {
+        let dialogTarget = document.getElementById('dialog')
+        dialogTarget.scrollTop = dialogTarget.scrollHeight
+      },
+
+      receivedNewMessage () {
+        const vueModel = this
+        return querySnapshot => {
+          let lastMessageDoc
+          let lastChange = querySnapshot.docChanges().last()
+          if (!lastChange) {
+            return
+          }
+
+          lastMessageDoc = lastChange.doc
+          if (lastChange.type === 'added') {
+            vueModel.$set(vueModel.messages, lastMessageDoc.id, lastMessageDoc.data())
+            vueModel.$nextTick(vueModel.scrollBottom)
+          }
+        }
+      },
+
+      triggerUploadImage () {
+        document.getElementById('upload-image').click()
+      },
+
+      paintCanvas (event) {
+        const vueModel = this
+        const originalImageFile = event.target.files[0]
+        const fileName = `${Math.floor(Date.now() / 1000)}_${originalImageFile.name}`
+        let reader
+
+        vueModel.isPreviewImage = true
+        vueModel.isShowImageProgress = true
+        vueModel.uploadImage(fileName, originalImageFile, uploadTask => {
+          return async () => {
+            vueModel.originalImageUrl = await uploadTask.snapshot.ref.getDownloadURL()
+            vueModel.isShowImageProgress = false
+          }
+        })
+
+        reader = new FileReader()
+        reader.readAsDataURL(originalImageFile)
+        reader.onload = event => {
+          const img = new Image()
+          img.src = event.target.result
+          img.onload = () => {
+            let canvasWidth, canvasHeight, canvasElement, context
+            if (img.width <= 240 && img.height <= 240) {
+              canvasWidth = img.width
+              canvasHeight = img.height
+            } else {
+              canvasWidth = (img.width > img.height) ? 240 : img.width / (img.height / 240)
+              canvasHeight = (img.height > img.width) ? 240 : img.height / (img.width / 240)
+            }
+            canvasElement = document.createElement('canvas')
+            canvasElement.width = canvasWidth
+            canvasElement.height = canvasHeight
+
+            context = canvasElement.getContext('2d')
+            context.drawImage(img, 0, 0, canvasWidth, canvasHeight)
+            context.canvas.toBlob(blob => {
+              const smallFileName = `small_${Math.floor(Date.now() / 1000)}_${originalImageFile.name}`
+              const smallFile = new File([blob], smallFileName, {
+                type: originalImageFile.type,
+                lastModified: Date.now()
+              })
+              vueModel.uploadImage(smallFileName, smallFile, uploadTask => {
+                return async () => {
+                  vueModel.smallImageUrl = await uploadTask.snapshot.ref.getDownloadURL()
+                }
+              })
+            }, originalImageFile.type, 1)
+          }
+        }
+        reader.onerror = console.error
+      },
+
+      uploadImage (fileName, imageFile, completeFn) {
+        const vueModel = this
+        const metadata = {
+          contentType: imageFile.type
+        }
+        const uploadTask = vueModel.storageRef.child(fileName).put(imageFile, metadata)
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+          snapshot => {},
+          error => {},
+          completeFn(uploadTask))
+      },
+
+      cancelImage () {
+        const vueModel = this
+        vueModel.originalImageUrl = ''
+        vueModel.isPreviewImage = false
+      },
+
       async retrieveMessages () {
         const vueModel = this
         if (vueModel.lineUserId) {
@@ -132,41 +240,46 @@
         eventBus.$emit('read', lineUserId)
       },
 
-      receivedNewMessage () {
+      async pushChatMessage (lineUserId, {
+        messageText = '使用者傳送一張圖片',
+        originalImageUrl = '',
+        smallImageUrl = ''
+      } = {}) {
         const vueModel = this
-        return querySnapshot => {
-          let lastMessageDoc
-          let lastChange = querySnapshot.docChanges().last()
-          if (!lastChange) {
-            return
-          }
-
-          lastMessageDoc = lastChange.doc
-          if (lastChange.type === 'added') {
-            vueModel.$set(vueModel.messages, lastMessageDoc.id, lastMessageDoc.data())
-            vueModel.$nextTick(vueModel.scrollBottom)
-          }
-        }
-      },
-
-      async pushChatMessage (lineUserId, messageText) {
-        const vueModel = this
-        return await vueModel
+        const OK_200 = 200
+        let response = await vueModel
           .axios({
             method: 'post',
             url: 'https://www.ehanlin.com.tw/linebot/admin/pushChatMessage',
             data: {
               lineUserId: lineUserId,
-              messageText: messageText
+              messageText: messageText,
+              originalContentUrl: originalImageUrl,
+              previewImageUrl: smallImageUrl
             }
           })
+
+        if (response.status === OK_200) {
+          vueModel.messageText = ''
+          await vueModel.updateMessagesToRead(lineUserId)
+          await vueModel.createMessage({
+            messageText: messageText,
+            originalImageUrl: originalImageUrl
+          })
+        } else {
+          showModal(vueModel, '傳送訊息失敗，請稍後再嘗試！')
+        }
       },
 
-      async createMessage (messageText) {
+      async createMessage (
+        {
+          messageText = '使用者傳送一張圖片',
+          originalImageUrl = '',
+        } = {}) {
         const vueModel = this
         let updateTime = await firebase.firestore.FieldValue.serverTimestamp()
         let loginUserInfo = vueModel.loginUserInfo
-        await db.collection('Messages').add({
+        let message = {
           lineUserId: vueModel.lineUserId,
           lineUserName: vueModel.lineUserName,
           lineUserAvatar: vueModel.lineUserAvatar,
@@ -176,63 +289,31 @@
           ehanlin: loginUserInfo.email,
           ehanlinName: loginUserInfo.name,
           ehanlinAvatar: loginUserInfo.avatar
-        })
-        //vueModel.retrieve()
-        //eventBus.$emit('retrieveChatMessage', lineUserId)
+        }
+
+        if (originalImageUrl) {
+          message.imageUrl = originalImageUrl
+        }
+
+        await db.collection('Messages').add(message)
       },
 
-      async submitMessage (event) {
+      async sentMessage (event) {
         const vueModel = this
-        const OK_200 = 200
-        let lineUserId = vueModel.lineUserId
-        let messageText = vueModel.messageText
-        let response
-
-        if (messageText === '') {
+        if (vueModel.messageText === '') {
           return
         }
-
         event.preventDefault()
-        response = await vueModel.pushChatMessage(lineUserId, messageText)
-        if (response.status === OK_200) {
-          vueModel.messageText = ''
-          await vueModel.updateMessagesToRead(lineUserId)
-          await vueModel.createMessage(messageText)
-        }
+        await vueModel.pushChatMessage(vueModel.lineUserId,
+          { messageText: vueModel.messageText })
       },
 
-      scrollBottom () {
-        let dialogTarget = document.getElementById('dialog')
-        dialogTarget.scrollTop = dialogTarget.scrollHeight
-      },
-
-      triggerUploadImage () {
-        document.getElementById('upload-image').click()
-      },
-
-      uploadImage (event) {
+      async sentImage () {
         const vueModel = this
-        let file = event.target.files[0]
-        let fileName = `${Math.floor(Date.now() / 1000)}_${file.name}`
-        let metadata = {
-          // cacheControl: 'public,max-age=300',
-          contentType: file.type
-        }
-        let uploadTask
-        vueModel.isPreviewImage = true
-        vueModel.isShowImageProgress = true
-        uploadTask = vueModel.storageRef.child(fileName).put(file, metadata)
-        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-          snapshot => {
-
-          },
-          error => {
-
-          },
-          async () => {
-            let downloadURL = await uploadTask.snapshot.ref.getDownloadURL()
-            vueModel.previewImageUrl = downloadURL
-            vueModel.isShowImageProgress = false
+        await vueModel.pushChatMessage(vueModel.lineUserId,
+          {
+            originalImageUrl: vueModel.originalImageUrl,
+            smallImageUrl: vueModel.smallImageUrl
           })
       }
     }
@@ -307,7 +388,7 @@
     }
 
     .chat-image-preview {
-      width: 130px;
+      width: 110px;
       object-fit: contain;
     }
 
@@ -315,9 +396,10 @@
       margin-bottom: -6px;
     }
 
-    .trigger-upload-image {
-      margin-top: 20px;
-      margin-left: -5px;
+    .image-control-icon {
+      position: relative;
+      top: 26px;
+      left: -5px;
       cursor: pointer;
     }
   }
